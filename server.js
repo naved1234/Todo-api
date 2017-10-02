75862317e846830bf5bb5e9f6a87d014a5cdb4e4
Naved <naved1234@gmail.com>
2017-10-02 16:14:42 +0530

    added few more changes to package.json and server.js

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
 var express=require('express');
 var app=express();
+var _=require('underscore');
 var bodyParser=require('body-parser');
 var PORT=process.env.PORT||3000;
 var todos=[];
 var todoNextId=1;
+
 app.use(bodyParser.json());
+
 app.get('/',function(req,res)
 {
   res.send('Todo API Root');
 });
+
 app.get('/todos',function(req,res)
 {
   res.json(todos);
 });
+
 app.get('/todos/:id',function(req,res)
 {
-  var todoId=req.params.id;
-  var matchedTodo;
-  for(var i=0;i<todos.length;i++)
-  {
-    if(todos[i].id==todoId)
-     {
-       matchedTodo=todos[i];
-       break;
-     }
-  }
-  if(typeof matchedTodo==='undefined')
-    res.status(404).send();
+  var todoId=parseInt(req.params.id,10);
+  var matchedTodo=_.findWhere(todos,{id:todoId});
+  if(matchedTodo)
+    res.send(matchedTodo);
   else
-    res.json(matchedTodo);
+    res.status(404).send();
 });
+
 app.post('/todos',function(req,res)
 {
-  var body=req.body;
+  var body=_.pick(req.body,'description','completed');
+  if(!_.isBoolean(body.completed)||!_.isString(body.description)
+    ||body.description.trim().length===0)
+    return res.status(400).send();
+  body.description=body.description.trim();
   body.id=todoNextId++;
   todos.push(body);
   res.json(body);
 });
+
 app.listen(PORT,function()
 {
   console.log('Server started at PORT: '+PORT+'!');
